307e7dfb34a161e3c7b5de4236c2cf07909e9b12
NASRELDIN-DIGIN\abdel <anasreldin@diginationmea.com>
2023-05-26 01:26:02 +0300

    Create Spring boot Application

---
+++ src/main/java/com/abdelrahman/nasr/eldin/expensestreacker/ExpensesTreackerApplication.java
@@ -0,0 +1,13 @@
+package com.abdelrahman.nasr.eldin.expensestreacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTreackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesTreackerApplication.class, args);
+	}
+
+}
